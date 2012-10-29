35567fd491822a141145a9f4abd9b51b9bbfa4b5
karamanov <karamanov@gmail.com>
2012-10-29 13:55:34 +0000

    Refactorred

---
+++ Belote/src/com/karamanov/beloteGame/message/Messageable.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) Dimitar Karamanov 2008-2010. All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the source code must retain
+ * the above copyright notice and the following disclaimer.
+ *
+ * This software is provided "AS IS," without a warranty of any kind.
+ */
+package com.karamanov.beloteGame.message;
+
+/**
+ * Messageable interface.
+ * @author Dimitar Karamanov
+ */
+public interface Messageable {
+
+    /**
+     * Performs message.
+     * @param message performing message.
+     */
+    void performMessage(Message message);
+}
