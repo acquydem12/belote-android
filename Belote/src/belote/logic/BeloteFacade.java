c6ef8570640761b59736b62e8caeb32fbd136c0c
karamanov <karamanov@gmail.com>
2012-11-07 08:38:24 +0000

    Refactorred

--- Belote/src/belote/logic/BeloteFacade.java
+++ Belote/src/belote/logic/BeloteFacade.java
@@ -176,6 +176,7 @@ public class BeloteFacade {
         game.setGameMode(GameMode.AnnounceGameMode);
         game.getAnnounceList().clear();
         game.getTrickList().clear();
+        game.clearHangedPoints();
         clearTeamsData();
         clearPlayersData();
         dealAnnounceCards();
