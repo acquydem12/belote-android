7e5ed4b7f0b15c005c67883cbe1158e390c3f2ac
karamanov <karamanov@gmail.com>
2013-03-06 09:54:22 +0000

    Refactorred

--- Belote/src/com/karamanov/beloteGame/Belote.java
+++ Belote/src/com/karamanov/beloteGame/Belote.java
@@ -16,20 +16,9 @@ import belote.bean.Game;
 import belote.logic.HumanBeloteFacade;
 
 import com.karamanov.framework.MessageApplication;
-import com.karamanov.framework.message.MessageType;
 
 public class Belote extends MessageApplication {
 
-    public final static MessageType MT_KEY_PRESSED = new MessageType("MT_KEY_PRESSED");
-
-    public final static MessageType MT_TOUCH_EVENT = new MessageType("MT_TOUCH_EVENT");
-
-    public final static MessageType MT_EXIT_EVENT = new MessageType("MT_EXIT_EVENT");
-
-    public final static MessageType MT_PAINT_EVENT = new MessageType("MT_PAINT_EVENT");
-    
-    public final static MessageType MT_CLOSE_END_GAME = new MessageType("MT_CLOSE_END_GAME");
-    
     /**
      * BELOTE_DAT constant.
      */
